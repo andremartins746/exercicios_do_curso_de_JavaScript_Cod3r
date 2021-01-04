1580a2a174963cdcbb159b6be485c5c33b7d75b2
André Martins Pereira <64427613+andremartins746@users.noreply.github.com>
2021-01-04 08:20:41 -0300

    exercicio de js

---
+++ lista_exercicios_resposta/exe3.js
@@ -0,0 +1,11 @@
+
+let skiils = ["javaScript", "React JS", "React Native"]
+function temHabilidades(skills1){
+    if(skiils.indexOf(skills1)){
+        console.log("true")
+    }else{
+        console.log('false')
+    }
+}
+
+temHabilidades("javaScript")
